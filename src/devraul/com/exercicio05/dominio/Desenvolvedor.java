77d15be4bae8e5ee1a732028b2286aa01ee0d885
raulmaciel <raulmacielxd@gmail.com>
2025-02-12 12:13:46 -0300

    feat: cria a classe Desenvolvedor que herda de Funcionario

---
+++ src/devraul/com/exercicio05/dominio/Desenvolvedor.java
@@ -0,0 +1,7 @@
+package devraul.com.exercicio05.dominio;
+
+public class Desenvolvedor extends Funcionario{
+    public Desenvolvedor(String nome, double salario) {
+        super(nome, salario);
+    }
+}
