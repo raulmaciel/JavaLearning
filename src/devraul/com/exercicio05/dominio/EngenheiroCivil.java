918c7c1a5b314126259bfc14b769413e5b5ce360
raulmaciel <raulmacielxd@gmail.com>
2025-02-12 12:14:18 -0300

    feat: cria a classe EngenheiroCivil que herda de Funcionario

---
+++ src/devraul/com/exercicio05/dominio/EngenheiroCivil.java
@@ -0,0 +1,7 @@
+package devraul.com.exercicio05.dominio;
+
+public class EngenheiroCivil extends Funcionario{
+    public EngenheiroCivil(String nome, double salario) {
+        super(nome, salario);
+    }
+}
